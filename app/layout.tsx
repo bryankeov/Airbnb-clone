5c338eaa11b52a2fd862c026cf5ba07a65572d59
Bryan <suprnewb@gmail.com>
2023-04-26 15:14:36 +1000

    Create navbar

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import './globals.css';
+import { Nunito } from 'next/font/google';
+import Navbar from "./components/navbar/Navbar";
+
+const font = Nunito({
+  subsets: ["latin"],
+})
+export const metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb clone using Next.js',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Navbar />
+        {children}
+        </body>
+    </html>
+  )
+}
