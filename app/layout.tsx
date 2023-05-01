2ee979c43bcb0a99f6c33224905eecc1abc8158f
Bryan <suprnewb@gmail.com>
2023-05-01 18:22:12 +1000

    Add Prisma

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Nunito } from "next/font/google";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
 
 const font = Nunito({
   subsets: ["latin"],
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToasterProvider />
         <RegisterModal />
         <Navbar />
         {children}
